63934f4d42e61719f9878ed1f72a7acce23570e8
Leonardo <leo.abreudp@gmail.com>
2023-06-27 11:09:06 +0100

    mini calculator

--- main/src/components/count_interno.jsx
+++ main/src/components/count_interno.jsx
@@ -1,7 +1,24 @@
+import React, { useState } from 'react';
+
 export default function CountInterno() {
+
+  const [count, setCount] = useState(0);
+
+  const buttons = [
+    {label: '+', action: ()=>{setCount(count+1)}},
+    {label: '-', action: ()=>{setCount(count-1)}},
+    {label: '^2', action: ()=>{setCount(count**2)}},
+    {label: 'AC', action: ()=>{setCount(0)}},
+  ];
+  
   return(
-    <div>
-      Div via React
+    <div className="flex flex-col space-y-4">
+      <output className="col-span-2">{count}</output>
+      <div className='space-x-4 [&>button]:px-4 [&>button]:text-white [&>button]:rounded [&>button]:bg-slate-500'>
+        {buttons.map((button, index) => (
+          <button key={index} onClick={button.action}>{button.label}</button>
+        ))}
+      </div>
     </div>
   );
 }
--- main/src/components/navbar.jsx
+++ main/src/components/navbar.jsx
@@ -2,7 +2,6 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 h-16 p-4 flex justify-start items-center w-full space-x-4">
       <h5>Leo React App</h5>
-      <a href="">Example 1</a>
     </nav>
   );
 }
